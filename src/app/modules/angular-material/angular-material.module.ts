a7cc1399c2ffdf84a32bb9cda81bbc14119f2ed5
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2022-03-19 22:04:19 +0530

    Customize Navbar - Reduce height - Add a spacer - Add file opener button icon - Add corresponding material modules

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ]
 })
 export class AngularMaterialModule { }
